d94248de94e970fe7cf85b7d0cd2c3807f833b62
alexanderhuahuacccapacc <alexander123huahuaccapa@gmail.com>
2025-10-28 18:44:37 -0500

    despierto_1130

--- ms-transport-tracking/src/main/java/org/example/mstransporttracking/entity/Location.java
+++ ms-transport-tracking/src/main/java/org/example/mstransporttracking/entity/Location.java
@@ -1,10 +1,7 @@
 package org.example.mstransporttracking.entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 
@@ -15,54 +12,21 @@ public class Location {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+
+    @Column(name = "vehicle_id", nullable = false)
     private String vehicleId;
 
     @JsonProperty("lat")
+    @Column(name = "latitude", nullable = false)
     private double latitude;
 
     @JsonProperty("lon")
+    @Column(name = "longitude", nullable = false)
     private double longitude;
 
+    @Column(name = "timestamp", nullable = false)
     private Long timestamp;
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getVehicleId() {
-        return vehicleId;
-    }
-
-    public void setVehicleId(String vehicleId) {
-        this.vehicleId = vehicleId;
-    }
-
-    public double getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-
-    public double getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
-
-    public Long getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(Long timestamp) {
-        this.timestamp = timestamp;
-    }
 
 }
