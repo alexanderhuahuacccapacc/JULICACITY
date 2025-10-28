dd51be75687240c61c3bc425b92cb988a726c752
alexanderhuahuacccapacc <alexander123huahuaccapa@gmail.com>
2025-10-28 15:36:25 -0500

    despierto_11

--- ms-transport-tracking/src/main/java/org/example/mstransporttracking/entity/Location.java
+++ ms-transport-tracking/src/main/java/org/example/mstransporttracking/entity/Location.java
@@ -1,5 +1,6 @@
 package org.example.mstransporttracking.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,8 +16,14 @@ public class Location {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @JsonProperty("id")
     private String vehicleId;
+
+    @JsonProperty("lat")
     private double latitude;
+
+    @JsonProperty("lon")
     private double longitude;
+
     private LocalDateTime timestamp;
 }
